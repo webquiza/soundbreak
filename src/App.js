605232eca8829d4536ae4f8876e6747cdeb4d10a
Carlos Urquiza <webquiza@gmail.com>
2022-02-15 19:59:08 -0800

    add initial iteration of functioning stopwatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// this import gives access to useState & useEffect hooks
+import React from "react";
+//import "./App.css";
 
 function App() {
+  // keep track of milliseconds timer has been on
+  const [time, setTime] = React.useState(0);
+  // checks if timer is on
+  const [timerOn, setTimeOn] = React.useState(false);
+
+  // useEffect hook will run everytime the timerOn variable changes
+  React.useEffect(() => {
+    // null because this will be same hook if we turn timer on or off
+    let interval = null;
+
+    // if timer is on we initialize interval
+    if (timerOn) {
+      // setInterval & clearInterval are a Javascript methods. Source: https://www.w3schools.com/jsref/met_win_setinterval.asp
+
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+      // if timer is off we clear interval
+    } else if (!timerOn) {
+      clearInterval(interval);
+    }
+
+    // clear interval set above
+    return () => clearInterval(interval);
+  }, [timerOn]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*displays time variable*/}
+      <div>{time}</div>
+
+      <div>
+        {/* when buttons are clicked, it sets timer to true or false*/}
+        <button onClick={() => setTimeOn(true)}>Start</button>
+        <button onClick={() => setTimeOn(false)}>Stop</button>
+        <button onClick={() => setTimeOn(true)}>Resume</button>
+
+        {/* when button is clicked, it sets timer to zero*/}
+        <button onClick={() => setTime(0)}>Reset</button>
+      </div>
     </div>
   );
 }
