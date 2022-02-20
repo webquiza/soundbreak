3767334aafadc971fc63958562cdcce94f39f2bd
Carlos Urquiza <webquiza@gmail.com>
2022-02-19 18:43:30 -0800

    move stopwatch feature into separate component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Stopwatch from "./Stopwatch";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Stopwatch />
+    </React.Fragment>
+  );
+}
+
+export default App;
