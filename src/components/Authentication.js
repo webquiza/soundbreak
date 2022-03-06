ef9c2fde6ce5a4c7eab83760cb38fc593fe47e57
Carlos Urquiza <webquiza@gmail.com>
2022-03-06 14:27:30 -0800

    validate access token prints to console

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 //import "./App.css";
 
 function App() {
@@ -5,6 +6,23 @@ function App() {
   const REDIRECT_URI = "http://localhost:3000";
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
+
+  const [token, setToken] = useState(" ");
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      console.log(token);
+    }
+  }, []);
 
   return (
     <div>
