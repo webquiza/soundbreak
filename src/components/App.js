25ecf1a82b0c2435535b08ca179d70670e97bc69
Carlos Urquiza <webquiza@gmail.com>
2022-03-11 22:08:21 -0800

    import additional components and bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Header from "./Header";
 import Stopwatch from "./Stopwatch";
 import Aunthentication from "./Authentication";
+import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <React.Fragment>
+      <Header />
       <Stopwatch />
       <Aunthentication />
+      <Footer />
     </React.Fragment>
   );
 }
