4094ec6f17141515fd98a590dd27552c6f428871
Carlos Urquiza <webquiza@gmail.com>
2022-03-11 22:11:39 -0800

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+const Footer = () => (
+  <div className="Footer">
+    <h5>Soundbreak | By Carlos Urquiza</h5>
+  </div>
+);
+
+export default Footer;
