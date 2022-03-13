932d16a2e9853ae4bb360e3b7a642adf41f0839b
Carlos Urquiza <webquiza@gmail.com>
2022-03-13 10:37:50 -0700

    remove some comments and change cy.visit URL to local host

--- cypress/integration/1-getting-started/soundbreak_spec.js
+++ cypress/integration/1-getting-started/soundbreak_spec.js
@@ -1,14 +1,10 @@
 describe("Visit Soundbreak", () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
+    //Use if testing locally. Doing so will cause circleCI pipeline to fail
+    cy.visit("http://localhost:3000/");
 
-    //Use if testing locally - this will cause circleCI pipeline to fail
-    //cy.visit("http://localhost:3000/");
-    //Use if you want to test with circleCI pipeline
-    cy.visit("https://soundbreak-us.firebaseapp.com/");
+    //Use if you want to test a successfull circleCI pipeline
+    //cy.visit("https://soundbreak-us.firebaseapp.com/");
   });
 
   it("user can see logo", () => {
